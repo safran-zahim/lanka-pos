286c0a74906da72e5a60bb144394302fd47155f1
safran zahim <safran1600014@gmail.com>
2026-03-26 05:38:36 +0530

    Fix CORS to allow Vercel frontend domain

--- src/app.ts
+++ src/app.ts
@@ -15,15 +15,8 @@ app.use((req, res, next) => {
     next();
 });
 
-const allowedOrigins = ['http://localhost:4173', 'http://localhost:5173', 'http://localhost:5174'];
 app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
